ed6a984b0b075d71ad634c59a5699382b44a914c
Eli Richey <eli@elirichey.com>
2022-03-25 21:31:58 -0500

    initial component is PlethoraCamera, App.js is now Example.js

--- config/devConfig.js
+++
@@ -1,3 +0,0 @@
-export default Config = {
-  API_URL: 'https://734f45e1bd11.ngrok.io',
-};
--- src/Example.js
+++ src/Example.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {Text} from 'react-native';
-import Config from '../config/devConfig';
 import PlethoraCamera from './PlethoraCamera';
-// import UploadHTTP from '../utilities/http/UploadHTTP';
 
 export default function App(props) {
   return (
     <PlethoraCamera
       upload={{
-        uploadUrl: `${Config.API_URL}/upload/s3`,
+        uploadUrl: ``,
         authToken: null,
         nameConvention: 'Plethora',
       }}
       saveToCameraRoll={true}
       // Lifecycle Events
-      // onRecordingFinished={payload => { return await UploadHTTP.uploadVideo(payload, null) }}
-      // onRecordingError={e => console.log('RECORDING_ERROR', e)}
-      // onTakePicture={p => console.log('PICTURE', p)}
-      // onUploadComplete={u => console.log('UPLOADED', u)}
-      // onUploadProgress={p => console.log(`UPLOADING... ${p}%`)}
-      // onUploadError={e => console.log('UPLOAD_ERROR', e)}
+      onRecordingFinished={payload => {
+        return await UploadHTTP.uploadVideo(payload, null);
+      }}
+      onRecordingError={e => console.log('RECORDING_ERROR', e)}
+      onTakePicture={p => console.log('PICTURE', p)}
+      onUploadComplete={u => console.log('UPLOADED', u)}
+      onUploadProgress={p => console.log(`UPLOADING... ${p}%`)}
+      onUploadError={e => console.log('UPLOAD_ERROR', e)}
       // Custom Icons
       icons={{
         takePictureIcon: <Text style={{color: '#00FF00'}}>Snap</Text>,
