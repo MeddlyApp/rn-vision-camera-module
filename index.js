3782dcd42af2a28b59a1d9d26b1cf8244b0de12a
Eli Richey <eli@elirichey.com>
2022-04-01 13:56:32 -0500

    updated index file and version number

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import 'react-native-gesture-handler';
+import PlethoraCamera from './src/PlethoraCamera';
 
+export default function Camera(props) {
+  return <PlethoraCamera {...props} />;
+}
+
+/*
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import PlethoraCamera from './src/PlethoraCamera';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => PlethoraCamera);
+*/
