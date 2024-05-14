88e182bc86c32f07ea1d30ba43219da28b7fcf97
Eli Richey <eli@elirichey.com>
2024-05-14 12:41:18 -0500

    version update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-/*/ Production
+// Production
 import 'react-native-gesture-handler';
 import MeddlyCamera from './src/MeddlyCamera';
 export default MeddlyCamera;
-/*/
+//
 
-// Testing
+/*/ Testing
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import Example from './src/Example';
 AppRegistry.registerComponent(appName, () => Example);
-//
+/*/
